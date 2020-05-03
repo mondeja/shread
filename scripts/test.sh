4ca22767f69f1e6561d5050dfbdaca2de5b89a79
Álvaro Mondéjar <mondejar1994@gmail.com>
2020-05-03 17:33:48 +0200

    Fix error on test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,8 +8,8 @@ if [ ! -f scripts/shunit2 ]; then
   bash public/unix/_/download/shunit/en.sh \
     --dest-path "scripts/shunit2" > /dev/null || exit $?
 fi;
-source scripts/shunit2
-printf "Using shunit2 v$SHUNIT_VERSION.\n"
+SHUNIT_VERSION=$(cat scripts/shunit2 | grep "SHUNIT_VERSION=" | cut -d"'" -f2)
+printf "Using shunit2 v$SHUNIT_VERSION\n\n"
 
 find src -iname "test.sh" | while read filepath; do
   bash "$filepath"
