259d8a31e4ed7307fb2f47785133cb60d0d5c6fb
Álvaro Mondéjar <mondejar1994@gmail.com>
2020-05-03 17:28:34 +0200

    Fix prints

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,6 +8,8 @@ if [ ! -f scripts/shunit2 ]; then
   bash public/unix/_/download/shunit/en.sh \
     --dest-path "scripts/shunit2" > /dev/null || exit $?
 fi;
+source scripts/shunit2
+printf "Using shunit2 v$SHUNIT_VERSION.\n"
 
 find src -iname "test.sh" | while read filepath; do
   bash "$filepath"
