aeb373223360f5c7a95a8f01900e1b722cfccc46
Álvaro Mondéjar <mondejar1994@gmail.com>
2020-05-19 22:41:11 +0200

    Add Debian Stretch support for tests.

--- src/unix/_/util/get-distro/test.sh
+++ src/unix/_/util/get-distro/test.sh
@@ -3,14 +3,10 @@
 : '
   Test if the distribution of linux is sourced on environment variables:
     - UNIX_DISTRO -> Distribution name lowercased.
-    - UNIX_DISTRO_CAPITALIZED -> Distribution name capitalized.
-
-    Currently, only Ubuntu distributions are supported.
 '
 testUnixDistro() {
   . public/unix/_/util/get-distro/en.sh
-  assertEquals "ubuntu" "$UNIX_DISTRO"
-  assertEquals "Ubuntu" "$UNIX_DISTRO_CAPITALIZED"
+  assertEquals "$SHREAD_TEST__UNIX_DISTRO" "$UNIX_DISTRO"
 }
 
 . ./scripts/shunit2
