ff65f12396d4fc86ca0e3926045760e0aa3ce491
Álvaro Mondéjar <mondejar1994@gmail.com>
2020-05-28 19:57:15 +0200

    Script 'src/unix/debian/install/atom/main.sh' linted.

--- src/unix/debian/install/atom/main.sh
+++ src/unix/debian/install/atom/main.sh
@@ -34,22 +34,22 @@ function printPrependedStdout() {
 }
 
 printPrependedStdout
-ATOM_BINARY_PATH=$(command -v atom)
+ATOM_BINARY_PATH="$(command -v atom)"
 if [ "$ATOM_BINARY_PATH" != "" ]; then
   ATOM_VERSION=$(
     apm -v | \
     sed -n 4p | \
     cut -d' ' -f2 | \
     sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g")
-  printf "Atom v$ATOM_VERSION $_MSG_ALREADY_INSTALLED"
+  printf "Atom v%s %s" "$ATOM_VERSION" "$_MSG_ALREADY_INSTALLED"
   printf " \e[92m\xE2\x9C\x94\e[39m\n"
 else
-  printf "$_MSG_CHECKING_ATOM\n"
+  printf "%s\n" "$_MSG_CHECKING_ATOM"
+
   INSTALLATION_DEPENDENCIES=(
     "wget"
     "jq"
   )
-
   for DEP in "${INSTALLATION_DEPENDENCIES[@]}"; do
     if [[ "$(dpkg -s "$DEP" 2> /dev/null | grep Status)" != "Status: install ok installed" ]]; then
       sudo apt-get install -y -qqq "$DEP" > /dev/null || exit $?
@@ -57,22 +57,22 @@ else
   done;
 
   printPrependedStdout
-  printf "  $_MSG_ADDING_REPO..."
+  printf "  %s..." "$_MSG_ADDING_REPO"
   SIGN_ATOM_GPG_KEY_STDERR=$(
     wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | \
     sudo apt-key add - 2>&1 > /dev/null)
   SIGN_ATOM_GPG_KEY_EXIT_CODE=$?
   if [ $SIGN_ATOM_GPG_KEY_EXIT_CODE -ne 0 ]; then
     printf " \e[91m\xE2\x9C\x95\e[39m\n" >&2
-    printf "\n$_MSG_ERROR_OBTAINING_ATOM_PUBLIC_KEY\n" >&2
-    printf "$SIGN_ATOM_GPG_KEY_STDERR\n" >&2
+    printf "\n%s\n" "$_MSG_ERROR_OBTAINING_ATOM_PUBLIC_KEY" >&2
+    printf "%s\n" "$SIGN_ATOM_GPG_KEY_STDERR" >&2
     exit $SIGN_ATOM_GPG_KEY_EXIT_CODE
   fi;
   sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
   printf " \e[92m\xE2\x9C\x94\e[39m\n"
 
   printPrependedStdout
-  printf "  $_MSG_UPDATING_PACKAGES"
+  printf "  %s" "$_MSG_UPDATING_PACKAGES"
   sudo apt-get update -qqq > /dev/null
   printf " \e[92m\xE2\x9C\x94\e[39m\n"
 
@@ -84,21 +84,22 @@ else
     cut -d' ' -f2)
   if [ "$ATOM_VERSION" != "" ]; then
     printPrependedStdout
-    printf "  $_MSG_ATOM_FOUND (v$ATOM_VERSION)"
+    printf "  %s (v%s)" "$_MSG_ATOM_FOUND" "$ATOM_VERSION"
     printf " \e[92m\xE2\x9C\x94\e[39m\n"
   fi;
 
   printPrependedStdout
-  printf "  $_MSG_RUNNING_INSTALLATION_SCRIPT"
-  INSTALL_ATOM_STDERR=$(sudo apt-get install atom -y -qqq 2>&1 > /dev/null)
+  printf "  %s" "$_MSG_RUNNING_INSTALLATION_SCRIPT"
+  INSTALL_ATOM_STDERR=$(
+    sudo apt-get install atom -y -qqq 2>&1 > /dev/null)
   INSTALL_ATOM_EXIT_CODE=$?
   if [ "$INSTALL_ATOM_EXIT_CODE" -ne 0 ]; then
     printf " \e[91m\xE2\x9C\x95\e[39m\n" >&2
-    printf "\n$_MSG_ERROR_INSTALLING_ATOM" >&2
+    printf "\n%s" "$_MSG_ERROR_INSTALLING_ATOM" >&2
     if [ "$ATOM_VERSION" != "" ]; then
-      printf " (v$ATOM_VERSION)" >&2
+      printf " (v%s)" "$ATOM_VERSION" >&2
     fi;
-    printf ":\n$INSTALL_ATOM_STDERR\n" >&2
+    printf ":\n%s\n" "$INSTALL_ATOM_STDERR" >&2
     exit $INSTALL_ATOM_EXIT_CODE
   fi;
   printf " \e[92m\xE2\x9C\x94\e[39m\n"
