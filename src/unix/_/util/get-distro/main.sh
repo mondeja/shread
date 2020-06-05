31c8ec6b54089754695e91a43b15eb3f13f4d4da
Álvaro Mondéjar <mondejar1994@gmail.com>
2020-06-05 14:26:44 +0200

    Lint 'src/unix/_/util/get-distro/main.sh' script.

--- src/unix/_/util/get-distro/main.sh
+++ src/unix/_/util/get-distro/main.sh
@@ -2,13 +2,14 @@
 # -*- ENCODING: UTF-8 -*-
 
 # If is available, use LSB identification
-if [ -f /etc/lsb-release -o -d /etc/lsb-release.d ]; then
+if [ -f /etc/lsb-release ] || [ -d /etc/lsb-release.d ]; then
   UNIX_DISTRO=$(lsb_release -i | cut -d: -f2 | sed s/'^\t'//)
 else
+  # shellcheck disable=SC2010
   UNIX_DISTRO=$(ls -d /etc/[A-Za-z]*[_-][rv]e[lr]* | grep -v "lsb" | cut -d'/' -f3 | cut -d'-' -f1 | cut -d'_' -f1)
 fi
 
-if [ "$(echo $(echo $(echo "$UNIX_DISTRO" | cut -c1-6) | cut -c1-6))" = "debian" ]; then
+if [ "$(echo "$UNIX_DISTRO" | cut -c1-6 | cut -c1-6)" = "debian" ]; then
   UNIX_DISTRO="debian"
 fi;
 
@@ -18,4 +19,5 @@ fi
 
 export UNIX_DISTRO
 
-export UNIX_DISTRO_CAPITALIZED="$(tr '[:lower:]' '[:upper:]' <<< ${UNIX_DISTRO:0:1})${UNIX_DISTRO:1}"
+UNIX_DISTRO_CAPITALIZED="$(tr '[:lower:]' '[:upper:]' <<< ${UNIX_DISTRO:0:1})${UNIX_DISTRO:1}"
+export UNIX_DISTRO_CAPITALIZED
