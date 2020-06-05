d30ed8d7a393b6a60b3757e80f3f16a953507bcc
Álvaro Mondéjar <mondejar1994@gmail.com>
2020-06-05 14:50:27 +0200

    Lint 'scripts/create-directory-tree.sh' script.

--- scripts/create-directory-tree.sh
+++ scripts/create-directory-tree.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-find src -type d | while read line; do
+find src -type d | while read -r line; do
   relative_path_to_folder_from_src="${line:4:${#line}}"
   path_to_folder_dest="public/$relative_path_to_folder_from_src"
 
@@ -9,7 +9,7 @@ find src -type d | while read line; do
     continue
   fi;
 
-  if [ ! -d $path_to_folder_dest ]; then
-    mkdir $path_to_folder_dest
+  if [ ! -d "$path_to_folder_dest" ]; then
+    mkdir "$path_to_folder_dest"
   fi;
 done
