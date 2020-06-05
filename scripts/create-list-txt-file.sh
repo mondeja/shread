6f3fd4a08d874a04e401b20c859edb33ee90110f
Álvaro Mondéjar <mondejar1994@gmail.com>
2020-06-05 14:48:39 +0200

    Lint 'scripts/create-list-txt-file.sh' script.

--- scripts/create-list-txt-file.sh
+++ scripts/create-list-txt-file.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# shellcheck source=scripts/constants.sh
 source scripts/constants.sh
 
 touch public/list.txt
@@ -10,7 +11,7 @@ EXTENTIONS=(
 )
 
 for ext in "${EXTENTIONS[@]}"; do
-  find public -type f -name "*.$ext" | while read script_filepath; do
+  find public -type f -name "*.$ext" | while read -r script_filepath; do
     relative_path_to_script_from_public="${script_filepath:7:${#script_filepath}}"
 
     echo "$PUBLIC_URL/$relative_path_to_script_from_public" >> public/list.txt
