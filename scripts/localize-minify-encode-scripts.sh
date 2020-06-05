fb4e093f2fc35bb8a548eae316b59a009fcc4710
Álvaro Mondéjar <mondejar1994@gmail.com>
2020-06-05 15:10:21 +0200

    Lint 'scripts/localize-minify-encode-scripts.sh' script.

--- scripts/localize-minify-encode-scripts.sh
+++ scripts/localize-minify-encode-scripts.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-. scripts/constants.sh
+# shellcheck source=scripts/constants.sh
+source scripts/constants.sh
 
 # Add english to SUPPORTED_LANGUAGES
 SUPPORTED_LANGUAGES+=('en')
 
-find src -type f -name '*.sh' | while read script_filepath; do
+find src -type f -name '*.sh' | while read -r script_filepath; do
   # Ignore scripts with other names than `main.sh`
-  script_filename=$(basename $script_filepath)
+  script_filename="$(basename "$script_filepath")"
   if [ "$script_filename" != "main.sh" ]; then
     continue
   fi;
@@ -18,7 +19,7 @@ find src -type f -name '*.sh' | while read script_filepath; do
   relative_path_to_script_from_src="${script_filepath:4:${#script_filepath}}"
   # Relative path to `main.sh` directories from `src/`
   relative_path_to_script_dir_from_src=$(
-    dirname $relative_path_to_script_from_src)
+    dirname "$relative_path_to_script_from_src")
 
   # Iterate for language codes
   for lang in "${SUPPORTED_LANGUAGES[@]}"; do
@@ -37,43 +38,46 @@ find src -type f -name '*.sh' | while read script_filepath; do
         line_output=$line
         if [[ $line = _MSG* ]]; then
           # Get msgid
+          # shellcheck disable=SC2206
           MSG_VARIABLE_SPLIT=(${line//\"/ })
           MSG_SPLITTED_LENGTH="${#MSG_VARIABLE_SPLIT[@]}"
           MSGID=""
-          for i in `seq 1 $MSG_SPLITTED_LENGTH`; do
+          # shellcheck disable=SC2006
+          for i in `seq 1 "$MSG_SPLITTED_LENGTH"`; do
             MSGID="${MSGID} ${MSG_VARIABLE_SPLIT[$i]}"
           done
           # Trim spaces at the beggining and the end
-          MSGID="$(echo $MSGID | sed 's/^ | *$//')"
+          # shellcheck disable=SC2001
+          MSGID="$(echo "$MSGID" | sed 's/^ | *$//')"
 
           # Get msgstr
-          MSGSTR_LINE=$(
-            cat $po_filepath | grep -A 1 "msgid \"$MSGID\"" | tail -n 1)
+          MSGSTR_LINE=$(< "$po_filepath" grep -A 1 "msgid \"$MSGID\"" | tail -n 1)
           MSGSTR_LINE_LENGHT=${#MSGSTR_LINE}
-          MSGSTR_LINE_NDEL="$(($MSGSTR_LINE_LENGHT-9))"
+          MSGSTR_LINE_NDEL="$((MSGSTR_LINE_LENGHT-9))"
           MSGSTR=${MSGSTR_LINE:8:$MSGSTR_LINE_NDEL}
 
           # Replace variable in for message
+          # shellcheck disable=SC2128
           line_output="${MSG_VARIABLE_SPLIT}\"${MSGSTR}\""
         fi;
-        echo "$line_output" >> $temp_localized_script
-      done < $script_filepath
+        echo "$line_output" >> "$temp_localized_script"
+      done < "$script_filepath"
     else
-      cat $script_filepath >> $temp_localized_script
+      cat "$script_filepath" >> "$temp_localized_script"
     fi;
 
     # Delete comments and empty lines
-    sed -i '/^[[:blank:]]*#/d;s/#.*//' $temp_localized_script
-    sed -i '/^$/d' $temp_localized_script
+    sed -i '/^[[:blank:]]*#/d;s/#.*//' "$temp_localized_script"
+    sed -i '/^$/d' "$temp_localized_script"
 
     path_to_script_dest="public/$relative_path_to_script_dir_from_src/$lang.sh"
-    rm -f $path_to_script_dest
-    touch $path_to_script_dest
-    echo "#!/bin/bash" >> $path_to_script_dest
+    rm -f "$path_to_script_dest"
+    touch "$path_to_script_dest"
+    echo "#!/bin/bash" >> "$path_to_script_dest"
 
     # Encode scripts as base64
-    echo "source <(printf '$(base64 $temp_localized_script)' | base64 -d)" \
-      >> $path_to_script_dest
+    echo "source <(printf '$(base64 "$temp_localized_script")' | base64 -d)" \
+      >> "$path_to_script_dest"
     rm -f "$temp_localized_script"
   done
 done
