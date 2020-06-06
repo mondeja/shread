a75905ec464cf15303104b7347f45e003e708789
Álvaro Mondéjar <mondejar1994@gmail.com>
2020-06-06 13:20:44 +0200

    Lint 'scripts/test.sh' script.

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,16 +8,17 @@ if [ ! -f scripts/shunit2 ]; then
   bash public/unix/_/download/shunit/en.sh \
     --dest-path "scripts/shunit2" > /dev/null || exit $?
 fi;
-SHUNIT_VERSION=$(cat scripts/shunit2 | grep "SHUNIT_VERSION=" | cut -d"'" -f2)
-printf "Using shunit2 v$SHUNIT_VERSION\n\n"
+SHUNIT_VERSION=$(< scripts/shunit2 grep "SHUNIT_VERSION=" | cut -d"'" -f2)
+printf "Using shunit2 v%s\n\n" "$SHUNIT_VERSION"
 
 _EXIT_CODE=0
 printf "%s\n" "$(find src -iname "test.sh")" > /tmp/files-to-test.txt
-while read filepath; do
+while read -r filepath; do
   printf "%s" "---------------------------------------------------------"
-  printf "\n\$ bash $filepath\n\n"
+  printf "\n\$ bash %s\n\n" "$filepath"
   bash "$filepath"
-  if [ $? -ne 0 ]; then
+  _TEST_EXIT_CODE=$?
+  if [ $_TEST_EXIT_CODE -ne 0 ]; then
     _EXIT_CODE=1
   fi;
 done < /tmp/files-to-test.txt;
