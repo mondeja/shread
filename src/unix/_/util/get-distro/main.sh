34215c0e9d5639e3ef2ba8b21a4471da6de9ff46
Álvaro Mondéjar <mondejar1994@gmail.com>
2020-06-06 17:40:41 +0200

    Fix error at get distribution utility script in Debian.

--- src/unix/_/util/get-distro/main.sh
+++ src/unix/_/util/get-distro/main.sh
@@ -9,7 +9,7 @@ else
   UNIX_DISTRO=$(ls -d /etc/[A-Za-z]*[_-][rv]e[lr]* | grep -v "lsb" | cut -d'/' -f3 | cut -d'-' -f1 | cut -d'_' -f1)
 fi
 
-if [ "$(echo "$UNIX_DISTRO" | cut -c1-6 | cut -c1-6)" = "debian" ]; then
+if [ "$(echo "$UNIX_DISTRO" | head -n 1)" = "debian" ]; then
   UNIX_DISTRO="debian"
 fi;
 
@@ -17,7 +17,7 @@ if [ "$UNIX_DISTRO" = "Ubuntu" ]; then
   UNIX_DISTRO="ubuntu"
 fi
 
-export UNIX_DISTRO
-
 UNIX_DISTRO_CAPITALIZED="$(tr '[:lower:]' '[:upper:]' <<< ${UNIX_DISTRO:0:1})${UNIX_DISTRO:1}"
+
+export UNIX_DISTRO
 export UNIX_DISTRO_CAPITALIZED
