7966e9777ac3bbcfdf40315b226405745d975e47
Álvaro Mondéjar <mondejar1994@gmail.com>
2020-06-06 13:17:41 +0200

    Lint 'scripts/move-no-sh-scripts.sh' script.

--- scripts/move-no-sh-scripts.sh
+++ scripts/move-no-sh-scripts.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Move files that are not Bash scripts to public directory
-find src -type f -not -name '*.sh' -not -name '*.po' -not -name '*.mo' | while read file; do
+find src -type f -not -name '*.sh' -not -name '*.po' -not -name '*.mo' | while read -r file; do
   relative_path_to_file_from_src="${file:4:${#file}}"
   path_to_file_dest="public/$relative_path_to_file_from_src"
 
-  cp $file $path_to_file_dest
+  cp "$file" "$path_to_file_dest"
 done
