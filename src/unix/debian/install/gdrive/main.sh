9ad78a813671057e991bdadf52ac700747815b5b
Álvaro Mondéjar <mondejar1994@gmail.com>
2020-06-13 01:11:07 +0200

    Update GoogleDrive script.

--- src/unix/debian/install/gdrive/main.sh
+++ src/unix/debian/install/gdrive/main.sh
@@ -43,10 +43,21 @@ function printIndent() {
 }
 
 function installScriptDependencies() {
+  if [ "$(command -v pacman)" = "" ]; then
+    SCRIPT_FILENAME="$(basename "$0")"
+    if [ "$SCRIPT_FILENAME" = "main.sh" ]; then
+      filepath="src/unix/_/download/pacapt/main.sh"
+      bash "$filepath" > /dev/null
+    else
+      url="https://mondeja.github.io/shread/unix/_/download/pacapt/$SCRIPT_FILENAME"
+      curl -sL "$url" | sudo bash - > /dev/null
+    fi;
+  fi;
+
   INSTALLATION_DEPENDENCIES=("curl" "jq")
   for DEP in "${INSTALLATION_DEPENDENCIES[@]}"; do
-    if [[ "$(sudo dpkg -s "$DEP" 2> /dev/null | grep Status)" != "Status: install ok installed" ]]; then
-      sudo apt-get install -y -qqq "$DEP" > /dev/null || exit $?
+    if [[ "$(sudo pacman -Qi "$DEP" 2> /dev/null | grep Status)" != "Status: install ok installed" ]]; then
+      sudo pacman -S "$DEP" > /dev/null || exit $?
     fi;
   done
 }
