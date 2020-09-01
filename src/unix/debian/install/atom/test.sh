af8dad76b8b5e04369091a2b51d803437c7c2b02
Álvaro Mondéjar <mondejar1994@gmail.com>
2020-09-01 18:18:55 +0200

    Trying to debug in TravisCI...

--- src/unix/debian/install/atom/test.sh
+++ src/unix/debian/install/atom/test.sh
@@ -3,16 +3,20 @@
 
 function testAtomIsNotInstalled() {
   _ATOM_BINARY_PATH="$(command -v atom)"
-  if [ "$_ATOM_BINARY_PATH" != "" ] && [ "$SHREAD_TEST__ATOM_MUST_BE_NOT_INSTALLED" != "true" ]; then
+  if [ -z "$_ATOM_BINARY_PATH" ] && [ "$SHREAD_TEST__ATOM_MUST_NOT_BE_INSTALLED" != "true" ]; then
     startSkipping
   fi;
+
+  printf "atom not found"
 
   # Package manager binary doesn't exists
   _APM_BINARY_PATH="$(command -v apm)"
   assertNull "$_APM_BINARY_PATH"
 
+  printf "apm not found"
+
   # Package repository must not exists
-  assertTrue "[ -f \"/etc/apt/sources.list.d/atom.list\" ]"
+  assertTrue "[ ! -f \"/etc/apt/sources.list.d/atom.list\" ]"
 }
 
 #function testAtomInstallation() {}
