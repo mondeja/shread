c1f7dda28cd75ce49b133c927c98143731ca017f
Álvaro Mondéjar <mondejar1994@gmail.com>
2020-09-01 20:11:39 +0200

    Add Atom installation test

--- src/unix/debian/install/atom/test.sh
+++ src/unix/debian/install/atom/test.sh
@@ -3,22 +3,39 @@
 
 function testAtomIsNotInstalled() {
   _ATOM_BINARY_PATH="$(command -v atom)"
-  if [ -z "$_ATOM_BINARY_PATH" ] && [ "$SHREAD_TEST__ATOM_MUST_NOT_BE_INSTALLED" != "true" ]; then
+  if [ -n "$_ATOM_BINARY_PATH" ] && [ "$SHREAD_TEST__ATOM_MUST_NOT_BE_INSTALLED" != "true" ]; then
     startSkipping
   fi;
 
-  printf "atom not found"
+  # package manager binary must not exists
+  assertNull \
+    "apm binary available before atom install" \
+    "$(command -v apm)"
 
-  # Package manager binary doesn't exists
-  _APM_BINARY_PATH="$(command -v apm)"
-  assertNull "$_APM_BINARY_PATH"
-
-  printf "apm not found"
-
-  # Package repository must not exists
-  assertTrue "[ ! -f \"/etc/apt/sources.list.d/atom.list\" ]"
+  # package repository must not exists
+  assertTrue \
+    "'/etc/apt/sources.list.d/atom.list' exists before atom install" \
+    "[ ! -f \"/etc/apt/sources.list.d/atom.list\" ]"
 }
 
-#function testAtomInstallation() {}
+function testAtomInstallation() {
+  if [ -n "$SHREAD_TEST__ATOM_SKIP_INSTALL" ]; then
+    startSkipping
+  fi;
+  sudo bash public/unix/debian/install/atom/en.sh
+
+  # atom and apm binaries must be available after installation
+  assertNotNull \
+    "atom binary not available after atom install" \
+    "$(command -v atom)"
+  assertNotNull \
+    "apm binary not available after atom install" \
+    "$(command -v atom)"
+
+  # package repository file must exists after installation
+  assertTrue \
+    "'/etc/apt/sources.list.d/atom.list' not exists after atom install" \
+    "[ -f \"/etc/apt/sources.list.d/atom.list\" ]"
+}
 
 . ./scripts/shunit2
