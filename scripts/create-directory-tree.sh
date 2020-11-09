219ce7a4aa1026cec79aeef85322f04761b17add
Álvaro Mondéjar <mondejar1994@gmail.com>
2020-11-09 11:42:05 +0100

    Add '--help' option to 'create-gemfile.sh' and 'create-directory-tree.sh' meta scripts.

--- scripts/create-directory-tree.sh
+++ scripts/create-directory-tree.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 # -*- ENCODING: UTF-8 -*-
 
+function usage {
+  printf "Usage: %s [-h]\n\n" "$0"
+  printf "  Creates the same tree directories structures of 'src/' into"
+  printf " 'public/' directory.\n\n"
+  printf "Options:\n"
+  printf "  -h, --help                    Show this help message and exit.\n"
+  exit 1
+}
+
+for arg in "$@"; do
+  case $arg in
+    -h|--help)
+    shift
+    usage
+    ;;
+  esac
+done
+
 find src -type d | while read -r line; do
   relative_path_to_folder_from_src="${line:4:${#line}}"
   path_to_folder_dest="public/$relative_path_to_folder_from_src"
--- scripts/create-gemfile.sh
+++ scripts/create-gemfile.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 # -*- ENCODING: UTF-8 -*-
 
+function usage {
+  printf "Usage: %s [-h]\n\n" "$0"
+  printf "  Creates a minimal Gemfile that only includes 'jekyll' gem in the"
+  printf " path 'public/Gemfile' to increase Jekyll's deployment speed.\n\n"
+  printf "Options:\n"
+  printf "  -h, --help                    Show this help message and exit.\n"
+  exit 1
+}
+
+for arg in "$@"; do
+  case $arg in
+    -h|--help)
+    shift
+    usage
+    ;;
+  esac
+done
+
 tee -a public/Gemfile > /dev/null << END
 # frozen_string_literal: true
 
